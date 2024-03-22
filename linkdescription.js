0ee2a50d72129cbee691ed72ddca09b049ed2c5d
Azryx / K4v4D / ZegoFr34ks <zegofr34ks@gmail.com>
2024-03-22 18:34:08 +0100

    added description for links

---
+++ linkdescription.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll('.link');
+    const descriptionBox = document.querySelector('.description-box');
+
+    links.forEach(link => {
+        link.addEventListener('mouseenter', function() {
+            const description = this.getAttribute('data-description');
+            descriptionBox.textContent = description;
+            
+            // Check if the screen width is greater than 768px before displaying the description box
+            if (window.innerWidth > 480) {
+                descriptionBox.style.display = 'block';
+            }
+        });
+
+        link.addEventListener('mouseleave', function() {
+            descriptionBox.style.display = 'none';
+        });
+    });
+});
