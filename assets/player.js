c38c5333484ece6bb061ecfee0bfc6713b42f8b9
Azryx / K4v4D / ZegoFr34ks <zegofr34ks@gmail.com>
2026-01-15 19:08:39 +0100

    version change

--- assets/player.js
+++ assets/player.js
@@ -2697,7 +2697,7 @@ New features include: Customization, Sorting/Filtering, Queuing, Liking, Crossfa
 Did you know you can swipe a song card to either queue or like them? Swipe -> queue | <- like
 
 
-LP3 Version: 202615010637
+LP3 Version: 202615010708
 
 Created By Azryx (Github source code: https://github.com/ZegoFr34ks/zegofr34ks.github.io )
 
